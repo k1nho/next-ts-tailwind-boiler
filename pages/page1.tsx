321deb2efccac52e5e90e2836667d4d897d421e6
kinho <kinhong99@gmail.com>
2021-09-19 16:42:00 -0400

    add basic GET endpoint

--- pages/page1.tsx
+++ pages/page1.tsx
@@ -1,16 +1,14 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { Container3 } from '../components/Container3'
-import { Container2 } from '../components/Container2'
-import { Navbar } from '../components/Navbar'
-import { useEffect, useState } from 'react'
-import { Dropdown } from '../components/Dropdown'
+import type { NextPage } from "next";
+import Head from "next/head";
+// @ts-ignore
+import { useEffect, useState } from "react";
+import { Container2 } from "../components/Container2";
+import { Dropdown } from "../components/Dropdown";
+import { Navbar } from "../components/Navbar";
 
-const page1: NextPage = () => 
-
-{
-    const [isOpen, setIsOpen] = useState(false);
+const page1: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState("");
 
   const toggle = () => {
     setIsOpen(!isOpen);
@@ -30,23 +28,31 @@ const page1: NextPage = () =>
       window.removeEventListener("resize", hideMenu);
     };
   });
+
+  useEffect(() => {
+    const getName = async () => {
+      const response = await fetch("/api/get", {
+        headers: { Accept: "application/json" },
+      });
+      const parseResponse = await response.json();
+      setUser(parseResponse.name);
+    };
+    getName();
+  }, [user]);
+
   return (
-    <div className= "min-h-screen ">
+    <div className="min-h-screen ">
       <Head>
         <title>page 1</title>
         <meta charSet="utf-8" />
         <meta name=" author" content="Kin Hong NG" />
-        <meta
-          name="description"
-          content=" page 1 description "
-        />
+        <meta name="description" content=" page 1 description " />
       </Head>
-      <Navbar toggle= {toggle} isOpen= {isOpen}/>
-      <Dropdown toggle={toggle} isOpen ={isOpen}/>
-     <Container2/>
-     This is page1
+      <Navbar toggle={toggle} isOpen={isOpen} />
+      <Dropdown toggle={toggle} isOpen={isOpen} />
+      <Container2 user={user} />
     </div>
-  )
-}
+  );
+};
 
-export default page1
+export default page1;
