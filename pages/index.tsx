7eab430b47d1c47d291391df4211df2446de4356
Ehtesham Siddiqui <siddiquie.ehtesham@gmail.com>
2021-09-25 11:42:32 +0530

    Dashboard and Login Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,30 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { Container3 } from '../components/Container3'
-import { Container2 } from '../components/Container2'
-import { Navbar } from '../components/Navbar'
-import { useEffect, useState } from 'react'
-import { Dropdown } from '../components/Dropdown'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { Container3 } from "../components/Container3";
+import { Container2 } from "../components/Container2";
+import { Navbar } from "../components/Navbar";
+import { useEffect, useState } from "react";
+import { Dropdown } from "../components/Dropdown";
+import Login from "../components/login";
 
-const Home: NextPage = () => 
-
-{
-
+const Home: NextPage = () => {
   return (
-    <div className= "min-h-screen ">
+    <div className="min-h-screen ">
       <Head>
-               <title>Home</title>
+        <title>Home</title>
         <meta charSet="utf-8" />
         <meta name=" author" content="author" />
-        <meta name="google-signin-client_id" content= {`${process.env.GOOGLE_CLIENT_ID}`}/>
         <meta
-          name="description"
-          content=" Page description "
+          name="google-signin-client_id"
+          content={`${process.env.GOOGLE_CLIENT_ID}`}
         />
-      </Head> 
-     <Container2/>
-     <Container3/> 
-    </div>
-  )
-}
+        <meta name="description" content=" Page description " />
+      </Head>
 
-export default Home
+      <Login />
+    </div>
+  );
+};
+
+export default Home;
